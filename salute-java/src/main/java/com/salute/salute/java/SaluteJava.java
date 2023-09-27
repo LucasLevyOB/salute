515376c92f417cab322d95fb6558e9f058172d91
LucasLevyOB <oliveiralucaslevy@gmail.com>
2023-09-26 23:15:02 -0300

    feat: criacao do projeto java usando maven

---
+++ salute-java/src/main/java/com/salute/salute/java/SaluteJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.salute.salute.java;
+
+/**
+ *
+ * @author lucas-levy
+ */
+public class SaluteJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
