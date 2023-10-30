e0822ebb2c353b5b2d3043331a56e59cab0a8a1e
danie <danielferdsdev@gmail.com>
2023-10-30 10:25:51 -0300

    create: tabela alocacao_sala_turma

--- salute-java/src/main/java/com/salute/salute/java/schemas/AlocacaoSalaTurma.java
+++ salute-java/src/main/java/com/salute/salute/java/schemas/AlocacaoSalaTurma.java
@@ -1,28 +1,52 @@
 package com.salute.salute.java.schemas;
 
-/*
-table salute.alocacao_sala_turma {
-  ast_sala integer [ref: > salute.sala.sal_id]
-  ast_turma integer [ref: > salute.turma.tur_id]
-  ast_horario integer [ref: > salute.horario.hor_id]
-  // tentar usar isso daqui para alocacao por outras pessoas
-  // criar outra tabela com alocador
-  ast_data date
-  ast_recorrente boolean
-  indexes {
-    (ast_sala, ast_turma, ast_horario) [pk]
-  }
-}
- */
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+//import java.sql.ResultSet;
+//import java.util.ArrayList;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.insertInto;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.deleteFrom;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.update;
+
+//import java.util.ArrayList;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+//import com.datastax.oss.driver.api.querybuilder.delete.Delete;
+import com.datastax.oss.driver.api.querybuilder.insert.RegularInsert;
+//import com.datastax.oss.driver.api.querybuilder.select.Select;
+//import com.datastax.oss.driver.api.querybuilder.select.Selector;
+//import com.datastax.oss.driver.api.querybuilder.select.Select;
+//import com.datastax.oss.driver.api.querybuilder.update.Update;
+//import com.salute.salute.java.Horario;
+//import com.datastax.oss.driver.api.querybuilder.update.UpdateStart;
+import com.salute.salute.java.database.ConnectionDB;
+//import com.salute.salute.java.database.ResultSetFunction;
+//import com.salute.salute.java.database.ResultSetFunction;
+//import com.salute.salute.java.enums.Semestre;
+//import com.salute.salute.java.recurso.Necessidade;
 
 public class AlocacaoSalaTurma {
-  public static String getSql() {
-    return "CREATE TABLE IF NOT EXISTS alocacao_sala_turma (" +
+  public static int createTable() {
+    String sql = "CREATE TABLE IF NOT EXISTS alocacao_sala_turma (" +
         " ast_sala INTEGER REFERENCES sala(sal_id)," +
         " ast_turma INTEGER REFERENCES turma(tur_id)," +
         " ast_horario INTEGER REFERENCES horario(hor_id)," +
-        " ast_data DATE," +
-        " ast_recorrente BOOLEAN," +
+        " ast_data date," +
+        " ast_recorrente boolean," +
         " PRIMARY KEY (ast_sala, ast_turma, ast_horario));";
+    return ConnectionDB.update(sql);
+  }
+
+  public static int insert(int sala, int turma, int horario, String data, boolean recorrente) {
+    RegularInsert query = insertInto("alocacao_sala_turma")
+        .value("ast_sala", literal(sala))
+        .value("ast_turma", literal(turma))
+        .value("ast_horario", literal(horario))
+        .value("ast_data", literal(data))
+        .value("ast_recorrente", literal(recorrente));
+    return ConnectionDB.update(query.toString());
   }
 }
