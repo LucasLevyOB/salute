c3c62eed9d9b590af3a3a06f73273ed81afcd357
danie <danielferdsdev@gmail.com>
2023-10-30 10:42:02 -0300

    create: tabela alocacao_recurso_sala

--- salute-java/src/main/java/com/salute/salute/java/schemas/AlocacaoRecursoSala.java
+++ salute-java/src/main/java/com/salute/salute/java/schemas/AlocacaoRecursoSala.java
@@ -1,18 +1,47 @@
 package com.salute.salute.java.schemas;
 
-/*
-table salute.alocacao_recurso_sala {
-  ars_id integer [pk, increment]
-  ars_recurso varchar [ref: - salute.recurso.rec_tombamento]
-  ars_sala integer [ref: - salute.sala.sal_id]
-}
- */
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+//import java.sql.ResultSet;
+//import java.util.ArrayList;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.insertInto;
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.deleteFrom;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.update;
+
+//import java.util.ArrayList;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+//import com.datastax.oss.driver.api.querybuilder.delete.Delete;
+import com.datastax.oss.driver.api.querybuilder.insert.RegularInsert;
+//import com.datastax.oss.driver.api.querybuilder.select.Select;
+//import com.datastax.oss.driver.api.querybuilder.select.Selector;
+//import com.datastax.oss.driver.api.querybuilder.select.Select;
+//import com.datastax.oss.driver.api.querybuilder.update.Update;
+//import com.salute.salute.java.Horario;
+//import com.datastax.oss.driver.api.querybuilder.update.UpdateStart;
+import com.salute.salute.java.database.ConnectionDB;
+//import com.salute.salute.java.database.ResultSetFunction;
+//import com.salute.salute.java.database.ResultSetFunction;
+//import com.salute.salute.java.enums.Semestre;
+//import com.salute.salute.java.recurso.Necessidade;
+
 
 public class AlocacaoRecursoSala {
-  public static String getSql() {
-    return "CREATE TABLE IF NOT EXISTS alocacao_recurso_sala (" +
+  public static int createTable() {
+    String sql = "CREATE TABLE IF NOT EXISTS alocacao_recurso_sala (" +
         " ars_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-        " ars_recurso VARCHAR(255) REFERENCES recurso(rec_tombamento)," +
-        " ars_sala INTEGER REFERENCES sala(sal_id));";
+        " ars_sala INTEGER REFERENCES sala(sal_id)," +
+        " ars_horario INTEGER REFERENCES horario(hor_id ))";
+    return ConnectionDB.update(sql);
   }
+
+  public static int insert(int sala, int horario) {
+    RegularInsert query = insertInto("alocacao_recurso_sala")
+        .value("ars_sala", literal(sala))
+        .value("ars_horario", literal(horario));
+    return ConnectionDB.update(query.toString());
+  } 
 }
