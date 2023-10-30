62d12d9ddb42abee7f619d2c7c19f03b1a85c169
danie <danielferdsdev@gmail.com>
2023-10-30 09:51:10 -0300

    create: tabela horario_turma

--- salute-java/src/main/java/com/salute/salute/java/schemas/HorarioTurma.java
+++ salute-java/src/main/java/com/salute/salute/java/schemas/HorarioTurma.java
@@ -1,20 +1,55 @@
 package com.salute.salute.java.schemas;
 
-/*
-table salute.horario_turma {
-  htu_turma integer [ref: > salute.turma.tur_id]
-  htu_horario integer [ref: > salute.horario.hor_id]
-  indexes {
-    (htu_turma, htu_horario) [pk]
-  }
-}
- */
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+//import java.sql.ResultSet;
+//import java.util.ArrayList;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.insertInto;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.deleteFrom;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.update;
+
+//import java.util.ArrayList;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.literal;
+//import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+//import com.datastax.oss.driver.api.querybuilder.delete.Delete;
+import com.datastax.oss.driver.api.querybuilder.insert.RegularInsert;
+//import com.datastax.oss.driver.api.querybuilder.select.Select;
+//import com.datastax.oss.driver.api.querybuilder.select.Selector;
+//import com.datastax.oss.driver.api.querybuilder.select.Select;
+//import com.datastax.oss.driver.api.querybuilder.update.Update;
+//import com.salute.salute.java.Horario;
+//import com.datastax.oss.driver.api.querybuilder.update.UpdateStart;
+import com.salute.salute.java.database.ConnectionDB;
+//import com.salute.salute.java.database.ResultSetFunction;
+//import com.salute.salute.java.database.ResultSetFunction;
+//import com.salute.salute.java.enums.Semestre;
+//import com.salute.salute.java.recurso.Necessidade;
 
 public class HorarioTurma {
-  public static String getSql() {
-    return "CREATE TABLE IF NOT EXISTS horario_turma (" +
-        " htu_turma INTEGER REFERENCES turma(tur_id)," +
-        " htu_horario INTEGER REFERENCES horario(hor_id)," +
-        " PRIMARY KEY (htu_turma, htu_horario));";
-  }
+ 
+    public static int createTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS horario_turma (" +
+            " htu_turma INTEGER REFERENCES turma(tur_id)," +
+            " htu_horario INTEGER REFERENCES horario(hor_id)," +
+            " PRIMARY KEY (htu_turma, htu_horario));";
+        return ConnectionDB.update(sql);
+    }
+
+    public static int insert(int turma, int horario) {
+        RegularInsert query = insertInto("horario_turma")
+            .value("htu_turma", literal(turma))
+            .value("htu_horario", literal(horario));
+        return ConnectionDB.update(query.toString());
+    }
+    
+    /*public static int update(int turma, int horario) {
+      String query = "UPDATE horario_turma "
+          + "SET htu_turma = " + turma + ", "
+          + "htu_horario = " + horario + " "
+          + "WHERE htu_turma = " + turma;
+      return ConnectionDB.update(query);
+    }*/
 }
