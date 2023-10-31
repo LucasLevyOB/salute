b072db58e61ff83920ef88e939c8630898db045e
Gabriel Ferreira <gabriel29@alu.ufc.br>
2023-10-31 12:20:16 -0300

    Add telas e fluxos

---
+++ salute-java/src/main/java/com/salute/salute/java/controller/CadastrarRecursos.java
@@ -0,0 +1,7 @@
+package com.salute.salute.java.controller;
+
+import javafx.fxml.Initializable;
+
+public class CadastrarRecursos extends Main {
+    
+}
---
+++ salute-java/src/main/java/com/salute/salute/java/controller/CadastrarTurmas.java
@@ -0,0 +1,5 @@
+package com.salute.salute.java.controller;
+
+public class CadastrarTurmas extends Main {
+    
+}
